c247f1792380699a05ef1c132ca763bd8608b314
20F20546-mec <118101059+20F20546-mec@users.noreply.github.com>
2022-11-13 12:41:04 +0400

    Created bill.java

---
+++ bill.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class bill {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Input the bill amount: ");
+        Double bill_price = sc.nextDouble();
+
+        System.out.println("Amount: " + bill_price);
+    }
+}
